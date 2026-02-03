ebc0bae8cb70cbdf0b6ed3c00747aaa759c4d43d
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-02-03 15:39:32 -0700

    Think i completed Content

--- components/content.tsx
+++ components/content.tsx
@@ -25,10 +25,35 @@ export default function Content() {
             <Text style={styles.gametext}>Portal 2</Text>
             <Text style={styles.pricetext}>CDN$ 20.00</Text>
           </View>
-        </View>
+        </View>{" "}
+        {/* featured ends here and discounts begins */}
       </View>
-      <View style={styles.discounttab}>
+      <View style={styles.eventtab}>
         <Text style={styles.genretext}>Discounts & Events</Text>
+        <View style={styles.listeventgame}>
+          <View style={styles.eventgame}>
+            <Image
+              source={require("../assets/images/steamimages/event1.jpg")}
+              style={styles.eventgameimage}
+            ></Image>
+            <View style={styles.eventgamebottom}>
+              <Text style={styles.eventtext}>MIDWEEK DEAL</Text>
+              <Text style={styles.offertext}>Offer ends 9 Feb @ 11:00am.</Text>
+              <Text style={styles.discounttext}>Up to -90%</Text>
+            </View>
+          </View>
+          <View style={styles.eventgame}>
+            <Image
+              source={require("../assets/images/steamimages/event2.png")}
+              style={styles.eventgameimage}
+            ></Image>
+            <View style={styles.eventgamebottom}>
+              <Text style={styles.eventtext}>MIDWEEK DEAL</Text>
+              <Text style={styles.offertext}>Offer ends 9 Feb @ 11:00am.</Text>
+              <Text style={styles.discounttext}>Up to -90%</Text>
+            </View>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -44,12 +69,12 @@ const styles = StyleSheet.create({
     color: "#ffffff",
     fontSize: 20,
     paddingTop: 20,
-    paddingLeft: 15,
+    paddingLeft: 10,
   },
   listgames: {
     paddingLeft: 14,
     marginTop: 8,
-    marginLeft: 7,
+    marginLeft: 3,
     display: "flex",
     flexDirection: "row",
   },
@@ -70,6 +95,7 @@ const styles = StyleSheet.create({
     color: "#ffffff",
     fontSize: 25,
     top: -4,
+    fontWeight: "500",
   },
   pricetext: {
     color: "#ffffff",
@@ -77,27 +103,57 @@ const styles = StyleSheet.create({
     top: 6,
     left: 15,
   },
-  listdiscountgame: {},
-  discounttab: {
+  listeventgame: {
+    marginTop: 8,
+    marginLeft: 3,
+    display: "flex",
+    flexDirection: "row",
+  },
+  eventtab: {
     top: 50,
     backgroundColor: "#1a2e46",
     width: "100%",
     height: 500,
+    paddingTop: 5,
   },
-  discountgame: {
+  eventgame: {
     marginRight: 15,
     left: 8,
     top: 10,
-    width: 350,
+    width: 300,
     boxShadow: "1px 3px 8px black",
   },
-  discountgameimage: {
-    height: 200,
+  eventgameimage: {
+    height: 300,
   },
-  discountgamebottom: {
-    backgroundImage: "linear-gradient(#000000, #223f5f)",
-    height: 80,
+  eventgamebottom: {
+    backgroundColor: "#1b5376",
+    height: 120,
     padding: 4,
+  },
+  eventtext: {
+    color: "#ffffff",
+    fontSize: 20,
+    top: 5,
+    left: 5,
+  },
+  offertext: {
+    color: "#DFDFDF",
+    fontSize: 11,
+    top: 5,
+    left: 7,
+  },
+  discounttext: {
+    color: "#b4e220",
+    backgroundColor: "#4b6b22",
+    width: 115,
+    fontSize: 20,
+    left: 20,
+    top: 25,
+    padding: 2,
+    paddingLeft: 5,
+    paddingRight: 4,
+    fontWeight: 500,
   },
 });
 
