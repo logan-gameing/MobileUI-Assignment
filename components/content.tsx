2cf46350add37baa77f52effc1feee3c23d77872
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-02-03 11:29:51 -0700

    Created Recommended tab

--- components/content.tsx
+++ components/content.tsx
@@ -1,11 +1,31 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Content() {
   return (
     <View style={styles.content}>
+      <Text style={styles.genretext}> Featured & recommended </Text>
       <View style={styles.listgames}>
-        <Text>hello</Text>
-        <Image src="../assets/images/steamimages/hl2.jpg"></Image>
+        <View style={styles.game}>
+          <Image
+            source={require("../assets/images/steamimages/hl2.jpg")}
+            style={styles.gameimage}
+          ></Image>
+          <View style={styles.gamebottom}>
+            <Text style={styles.gametext}>Half-Life 2</Text>
+            <Text style={styles.pricetext}>CDN$ 20.00</Text>
+          </View>
+        </View>
+        <View style={styles.game}>
+          <Image
+            source={require("../assets/images/steamimages/portal2.jpg")}
+            style={styles.gameimage}
+          ></Image>
+          <View style={styles.gamebottom}>
+            <Text style={styles.gametext}>Portal 2</Text>
+            <Text style={styles.pricetext}>CDN$ 20.00</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -16,11 +36,45 @@ const styles = StyleSheet.create({
     backgroundImage: "linear-gradient(#18455a, #223f5f)",
     width: "100%",
     height: "100%",
+    paddingTop: 20,
+    paddingLeft: 15,
   },
-  game: {},
-  listgames: {},
-  genretext: {},
-  pricetext: {},
+  genretext: {
+    color: "#ffffff",
+    fontSize: 20,
+  },
+  listgames: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  discounttab: {},
+  listdiscountgame: {},
+  game: {
+    marginRight: 15,
+    left: 8,
+    top: 10,
+    width: 350,
+    boxShadow: "1px 3px 8px black",
+  },
+  gameimage: {
+    height: 200,
+  },
+  gamebottom: {
+    backgroundImage: "linear-gradient(#000000, #223f5f)",
+    height: 80,
+    padding: 4,
+  },
+  gametext: {
+    color: "#ffffff",
+    fontSize: 25,
+    top: -4,
+  },
+  pricetext: {
+    color: "#ffffff",
+    fontSize: 12,
+    top: 5,
+    left: 15,
+  },
 });
 
 // steam gradient left to right
