6108609d8160c9aaa0d49a5d864b5d3810c3c3e5
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-02-03 10:48:52 -0700

    Created steam gradient

--- components/content.tsx
+++ components/content.tsx
@@ -1,5 +1,28 @@
-import { View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function Content() {
-  return <View></View>;
+  return (
+    <View style={styles.content}>
+      <View style={styles.listgames}>
+        <Text>hello</Text>
+        <Image src="../assets/images/steamimages/hl2.jpg"></Image>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  content: {
+    backgroundImage: "linear-gradient(#18455a, #223f5f)",
+    width: "100%",
+    height: "100%",
+  },
+  game: {},
+  listgames: {},
+  genretext: {},
+  pricetext: {},
+});
+
+// steam gradient left to right
+//#18455a left
+//#223f5f right
