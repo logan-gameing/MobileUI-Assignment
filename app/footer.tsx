41fe9bcaede0e5538f964e6bb619372ae940ef8c
Alex Dea <alex.anderdea@hotmail.com>
2026-02-05 14:51:29 -0700

    got footer rendering, now just need to make it look normal

---
+++ app/footer.tsx
@@ -0,0 +1,43 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+
+const Footer = () => {
+    return (
+    <View style = {styles.footerContainer}>
+        <View style = {styles.footerItem}>
+            <AntDesign name="tag" size={24} color="white" />
+        </View>
+        <View style = {styles.footerItem}>
+            <Entypo name="news" size={24} color="white" />
+        </View>
+        <View style = {styles.footerItem}>
+            <Ionicons name="shield" size={24} color="white" />
+        </View>
+        <View style = {styles.footerItem}>
+            <AntDesign name="bell" size={24} color="white" />
+        </View>
+        <View style = {styles.footerItem}>
+            <AntDesign name="bars" size={24} color="white" />
+        </View>
+    </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    footerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#1f2124',
+        width: '100%'
+
+    },
+    footerItem: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+export default Footer;
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-
+import Footer from './footer';
 export default function Index() {
   return (
     <View
@@ -10,6 +10,8 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Footer />
     </View>
+    
   );
 }
